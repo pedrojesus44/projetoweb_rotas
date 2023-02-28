519e82cf4d77c991f85dc654fbc2115be980bc4b
pedrojesus44 <pedro.jesus44@etec.sp.gov.br>
2023-02-28 10:23:22 -0300

    primeiro

---
+++ resources/views/contato.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contato</title>
+</head>
+<body>
+    <h1>Contato</h1>
+
+    <a href="{{ url('/') }}"><button>Home</button></a>    
+    <a href="{{ url('/produtos') }}"><button>Produtos</button></a>
+    <a href="{{ url('/empresa') }}"><button>Empresa</button></a>
+    <a href="{{ url('/contato') }}"><button>Contato</button></a>
+</body>
+</html>
